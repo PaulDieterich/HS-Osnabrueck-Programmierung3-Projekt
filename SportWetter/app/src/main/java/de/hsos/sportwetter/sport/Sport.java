10a5372600a3c8ee56cc0b716b99c8d9282511d0
Paul Dieterich <Paul.Dieterich0@gmail.com>
2021-01-29 09:39:48 +0100

    create Sport and SportDao

--- SportWetter/app/src/main/java/de/hsos/sportwetter/sport/Sport.java
+++ SportWetter/app/src/main/java/de/hsos/sportwetter/sport/Sport.java
@@ -1,2 +1,64 @@
-package de.hsos.sportwetter.sport;public class Sport {
+package de.hsos.sportwetter.sport;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.UUID;
+/**
+ * @author Paul Dieterich
+ * sport klasse für die erstellung von sport Objekten
+ * ebenso eine emplementierung von androidx.room.entity
+ */
+
+@Entity(tableName = "SPORT")
+public class Sport {
+    @PrimaryKey
+    @ColumnInfo(name = "sport_id")
+    private UUID sportID;
+    @ColumnInfo(name = "sport_name")
+    private String name;
+    @ColumnInfo(name = "min_number_of_participants")
+    private int minNumberOfParticipants;
+    @ColumnInfo(name = "max_number_of_participants")
+    private int maxNumberOfParticipants;
+
+    public Sport(UUID sportID, String name, int min_anzahl, int max_anzahl) {
+        this.sportID = sportID;
+        this.name = name;
+        this.minNumberOfParticipants = min_anzahl;
+        this.maxNumberOfParticipants = max_anzahl;
+    }
+
+    public UUID getSportID() {
+        return sportID;
+    }
+
+    public void setSportID(UUID sportID) {
+        this.sportID = sportID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMinNumberOfParticipants() {
+        return minNumberOfParticipants;
+    }
+
+    public void setMinNumberOfParticipants(int minNumberOfParticipants) {
+        this.minNumberOfParticipants = minNumberOfParticipants;
+    }
+
+    public int getMaxNumberOfParticipants() {
+        return maxNumberOfParticipants;
+    }
+
+    public void setMaxNumberOfParticipants(int maxNumberOfParticipants) {
+        this.maxNumberOfParticipants = maxNumberOfParticipants;
+    }
 }
--- SportWetter/app/src/main/java/de/hsos/sportwetter/sport/SportDao.java
+++ SportWetter/app/src/main/java/de/hsos/sportwetter/sport/SportDao.java
@@ -1,2 +1,33 @@
-package de.hsos.sportwetter.sport;public class SportDao {
+package de.hsos.sportwetter.sport;
+
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Transaction;
+import androidx.room.Update;
+
+import java.util.List;
+
+/**
+ * @author Paul Dieterich
+ * Dao für Sport
+ */
+
+
+@Dao
+public interface SportDao {
+    @Insert
+    void insertSport(Sport sport);
+
+    @Update
+    void updateSport(Sport sport);
+
+    @Delete
+    void deleteSport(Sport... sports);
+
+    @Query("SELECT * FROM Sport")
+    public List<Sport> getAllSports();
+
 }
