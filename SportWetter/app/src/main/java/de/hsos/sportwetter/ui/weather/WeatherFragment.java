b323217011b314937bd639cc5856d45946fcf85b
Yesaja <steffwaschk@msn.com>
2021-02-08 21:47:57 +0100

    formatting + background weather check

--- SportWetter/app/src/main/java/de/hsos/sportwetter/ui/weather/WeatherFragment.java
+++ SportWetter/app/src/main/java/de/hsos/sportwetter/ui/weather/WeatherFragment.java
@@ -10,24 +10,20 @@ import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.os.Handler;
-import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.SearchView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import net.aksingh.owmjapis.api.APIException;
 import net.aksingh.owmjapis.core.OWM;
 import net.aksingh.owmjapis.model.CurrentWeather;
 
 import de.hsos.sportwetter.R;
+import de.hsos.sportwetter.classes.weather.City;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -38,27 +34,24 @@ import de.hsos.sportwetter.R;
 public class WeatherFragment extends Fragment {
 
     private static final int INTERNET_PERMISSION = 100;
-    OWM owm;
-    CurrentWeather cwd;
-    private Handler handler;
-    TextView stadtname, land, avgTemp, minTemp, maxTemp;
-    Button addBtn;
+    private OWM owm;
+    private CurrentWeather cwd;
+    private City aktuelleStadt;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    NavController navController;
     public WeatherFragment() {
         // Required empty public constructor
     }
 
     public static WeatherFragment newInstance(String param1, String param2) {
-        WeatherFragment fragment = new WeatherFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
+        WeatherFragment fragment = new WeatherFragment();
         fragment.setArguments(args);
         return fragment;
     }
@@ -67,13 +60,8 @@ public class WeatherFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
         this.owm = new OWM(getString(R.string.openweather_api_key));
         owm.setUnit(OWM.Unit.METRIC);
-        this.cwd = new CurrentWeather();
-        this.handler = new Handler();
     }
 
     @Override
@@ -81,12 +69,12 @@ public class WeatherFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_weather, container, false);
-        this.addBtn = (Button) view.findViewById(R.id.add_btn);
-        this.stadtname = (TextView) view.findViewById(R.id.stadtname);
-        this.land = (TextView) view.findViewById(R.id.land);
-        this.avgTemp = (TextView) view.findViewById(R.id.avgTemp);
-        this.minTemp = (TextView) view.findViewById(R.id.minTemp);
-        this.maxTemp = (TextView) view.findViewById(R.id.maxTemp);
+        Button addBtn = (Button) view.findViewById(R.id.add_btn);
+        TextView stadtname = (TextView) view.findViewById(R.id.stadtname);
+        TextView land = (TextView) view.findViewById(R.id.land);
+        TextView avgTemp = (TextView) view.findViewById(R.id.avgTemp);
+        TextView minTemp = (TextView) view.findViewById(R.id.minTemp);
+        TextView maxTemp = (TextView) view.findViewById(R.id.maxTemp);
 
         if(!(ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED)) {
             ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.INTERNET}, INTERNET_PERMISSION);
@@ -100,19 +88,17 @@ public class WeatherFragment extends Fragment {
         maxTemp.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
 
         //TODO: Aufruf der Logik, vielleicht als Background Task
-        //Thread thread = new Thread(() -> weather.wetterAbfrage());
-        //thread.stadt();
 
-        /*
+        Handler handler = new Handler();
         Runnable runnableCode = new Runnable() {
             @Override
             public void run() {
-                weather.wetterAbfrage(stadtsuche.getQuery().toString());
-                stadtname.setText(weather.getStadtname());
-                land.setText(weather.getLand());
-                avgTemp.setText(weather.getTempAvg() + weather.getUnit());
-                minTemp.setText(weather.getTempMin() + weather.getUnit());
-                maxTemp.setText(weather.getTempMax() + weather.getUnit());
+                Resources res = getResources();
+                aktuelleStadt.getName();
+                aktuelleStadt.getLand();
+                avgTemp.setText(String.format(res.getString(R.string.temperature),cwd.getMainData().getTemp()));
+                maxTemp.setText(String.format(res.getString(R.string.temperature),cwd.getMainData().getTempMax()));
+                minTemp.setText(String.format(res.getString(R.string.temperature),cwd.getMainData().getTempMin()));
                 // Log.d("Handlers", "Called on main thread");
                 // Repeat this the same runnable code block again another 5 seconds
                 // 'this' is referencing the Runnable object
@@ -120,12 +106,15 @@ public class WeatherFragment extends Fragment {
             }
         };
         handler.post(runnableCode);
-         */
 
         addBtn.setOnClickListener(v -> {
             Navigation.findNavController(v).navigate(R.id.action_weatherFragment_to_addNewWeatherLocationFragment);
         });
 
         return view;
+    }
+
+    private void setAktuelleStadt(City auswahlStadt) {
+        this.aktuelleStadt = auswahlStadt;
     }
 }
