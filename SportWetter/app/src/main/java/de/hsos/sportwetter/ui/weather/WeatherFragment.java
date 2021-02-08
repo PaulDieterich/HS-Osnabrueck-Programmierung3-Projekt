b7f7a146499c01bc0e1c87764c11ea29fe510372
Yesaja <steffwaschk@msn.com>
2021-02-08 18:32:09 +0100

    OWD now directly in fragment, new string formats

--- SportWetter/app/src/main/java/de/hsos/sportwetter/ui/weather/WeatherFragment.java
+++ SportWetter/app/src/main/java/de/hsos/sportwetter/ui/weather/WeatherFragment.java
@@ -1,13 +1,12 @@
 package de.hsos.sportwetter.ui.weather;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.RequiresApi;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -16,23 +15,19 @@ import androidx.navigation.Navigation;
 
 import android.os.Handler;
 import android.os.StrictMode;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import net.aksingh.owmjapis.api.APIException;
+import net.aksingh.owmjapis.core.OWM;
+import net.aksingh.owmjapis.model.CurrentWeather;
 
-import de.hsos.sportwetter.MainActivity;
-import de.hsos.sportwetter.MainFragment;
 import de.hsos.sportwetter.R;
-import de.hsos.sportwetter.classes.weather.Weather;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -40,14 +35,15 @@ import de.hsos.sportwetter.classes.weather.Weather;
  * create an instance of this fragment.
  */
 @RequiresApi(api = Build.VERSION_CODES.M)
-public class WeatherFragment extends Fragment {
+public class WeatherFragment extends Fragment implements SearchView.OnQueryTextListener {
 
     private static final int INTERNET_PERMISSION = 100;
-    private Weather weather;
+    OWM owm;
+    CurrentWeather cwd;
     private Handler handler;
+    TextView stadtname, land, avgTemp, minTemp, maxTemp;
+    SearchView stadtsuche;
     Button addBtn;
-
-
 
     NavController navController;
     public WeatherFragment() {
@@ -68,7 +64,9 @@ public class WeatherFragment extends Fragment {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        this.weather = new Weather(getString(R.string.openweather_api_key));
+        this.owm = new OWM(getString(R.string.openweather_api_key));
+        owm.setUnit(OWM.Unit.METRIC);
+        this.cwd = new CurrentWeather();
         this.handler = new Handler();
     }
 
@@ -78,13 +76,14 @@ public class WeatherFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_weather, container, false);
         addBtn = (Button) view.findViewById(R.id.add_btn);
-        TextView stadtname = (TextView) view.findViewById(R.id.stadtname);
-        TextView land = (TextView) view.findViewById(R.id.land);
-        TextView avgTemp = (TextView) view.findViewById(R.id.avgTemp);
-        TextView minTemp = (TextView) view.findViewById(R.id.minTemp);
-        TextView maxTemp = (TextView) view.findViewById(R.id.maxTemp);
-        Button addBtn = (Button) view.findViewById(R.id.add_btn);
-        SearchView stadtsuche = (SearchView) view.findViewById(R.id.stadtsuche);
+        this.stadtname = (TextView) view.findViewById(R.id.stadtname);
+        this.land = (TextView) view.findViewById(R.id.land);
+        this.avgTemp = (TextView) view.findViewById(R.id.avgTemp);
+        this.minTemp = (TextView) view.findViewById(R.id.minTemp);
+        this.maxTemp = (TextView) view.findViewById(R.id.maxTemp);
+        this.addBtn = (Button) view.findViewById(R.id.add_btn);
+        this.stadtsuche = (SearchView) view.findViewById(R.id.stadtsuche);
+        stadtsuche.setOnQueryTextListener(this);
 
         if(ContextCompat.checkSelfPermission(
             this.getContext(), Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
@@ -105,15 +104,17 @@ public class WeatherFragment extends Fragment {
         //TODO: Aufruf der Logik, vielleicht als Background Task
         //Thread thread = new Thread(() -> weather.wetterAbfrage());
         //thread.stadt();
+
+        /*
         Runnable runnableCode = new Runnable() {
             @Override
             public void run() {
                 weather.wetterAbfrage(stadtsuche.getQuery().toString());
                 stadtname.setText(weather.getStadtname());
                 land.setText(weather.getLand());
-                avgTemp.setText(Double.toString(weather.getTempAvg()) + " °C");
-                minTemp.setText(Double.toString(weather.getTempMin()) + " °C");
-                maxTemp.setText(Double.toString(weather.getTempMax()) + " °C");
+                avgTemp.setText(weather.getTempAvg() + weather.getUnit());
+                minTemp.setText(weather.getTempMin() + weather.getUnit());
+                maxTemp.setText(weather.getTempMax() + weather.getUnit());
                 // Log.d("Handlers", "Called on main thread");
                 // Repeat this the same runnable code block again another 5 seconds
                 // 'this' is referencing the Runnable object
@@ -121,18 +122,43 @@ public class WeatherFragment extends Fragment {
             }
         };
         handler.post(runnableCode);
-
+         */
         addBtn.setOnClickListener(v -> {
             Navigation.findNavController(v).navigate(R.id.action_weatherFragment_to_addNewWeatherLocationFragment);
-
         });
 
-
-
-        // Inflate the layout for this fragment
         return view;
     }
 
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Resources res = getResources();
+        try {
+            cwd = owm.currentWeatherByCityName(query);
+            if(cwd.hasRespCode() && cwd.getRespCode() == 200) {
+                if(cwd.hasCityName()) {
+                    stadtname.setText(cwd.getCityName());
+                    land.setText(cwd.getSystemData().getCountryCode());
+                    minTemp.setText(String.format(res.getString(R.string.temperature),cwd.getMainData().getTempMin()));
+                    maxTemp.setText(String.format(res.getString(R.string.temperature),cwd.getMainData().getTempMax()));
+                    avgTemp.setText(String.format(res.getString(R.string.temperature),cwd.getMainData().getTemp()));
+                    return true;
+                } else {
+                    Toast.makeText(this.getActivity(),"Stadt nicht gefunden.",Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            } else {
+                Toast.makeText(this.getActivity(),"OpenWeather nicht erreichbar - bitte später erneut versuchen!.",Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        } catch (APIException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
-
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
 }
