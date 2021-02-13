a30eb4e18fd0c63e320230c73f7fbd172c0c0e70
Paul Dieterich <Paul.Dieterich0@gmail.com>
2021-02-13 13:08:24 +0100

    splachscreen

--- SportWetter/app/src/main/java/de/hsos/sportwetter/SplashScreenActivity.java
+++ SportWetter/app/src/main/java/de/hsos/sportwetter/SplashScreenActivity.java
@@ -7,6 +7,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Handler;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -23,23 +24,33 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-       if(Preferences.getInstance(this).getUser() == null){
+        setContentView(R.layout.activity_splash_screen);
+        int stop = 10000;
+        /**
+        *
+        * Handler() ist deprecation, aber das war die schnellste und einfachste lösung auf die ich
+        * gekommen bin
+        * */
+        @SuppressWarnings("deprecation") Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                scheduleSplachscreen();
+            }
+        },stop);
+    }
+    private void scheduleSplachscreen(){
+        if(Preferences.getInstance(this).getUser() == null){
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
             finish();
             //zu login weiterleiten
-       }else{
+        }else{
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
-       }
+            finish();
+        }
 
     }
-
 
 }
